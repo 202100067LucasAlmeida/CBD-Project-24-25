7ee8a92724c7570afcc4b7db8b2862b2acd7dbb1
202100067LucasAlmeida <202100067@estudantes.ips.pt>
2024-11-11 18:20:12 +0000

    AAAAAAAA

--- populates.sql
+++ populates.sql
@@ -3,8 +3,14 @@ use AdventureWorksBetter;
 -- Table Title --
 -- Why? beacause it too much simple
 select * from title;
-select distinct Title from AdventureWorksLegacy.dbo.Customer$;
+select distinct c.Gender from AdventureWorksLegacy.dbo.Customer$ c;
 
 insert into title(title_description)
 select distinct Title from AdventureWorksLegacy.dbo.Customer$ c
 where c.Title != '';
+
+-- ja está essa poha
+
+-- Table Gender --
+-- Why? I don't KNOW!
+select * from gender;
