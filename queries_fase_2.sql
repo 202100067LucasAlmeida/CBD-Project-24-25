95d87afac9b260865df1611c3da7c129c46ef2c8
João Pedro <202001541@estudantes.ips.pt>
2025-01-10 18:19:22 +0000

    feat: query pesquisa de vendas por cidade (maybe?)

--- queries_fase_2.sql
+++ queries_fase_2.sql
@@ -1,10 +1,15 @@
 use AdventureWorks;
-/*
+
+-- comandos para monitorar desempenho
+
+SET STATISTICS IO ON;
+SET STATISTICS TIME ON;
+
 -- Pesquisa de vendas por cidade
 select distinct
 	c.city_name as 'city', 
 	s.state_code as 'state code',
-	sum(count(sl.sales_id)) as 'total sales'
+	count(sl.sales_id) as 'total sales'
 from territory.city c
 	inner join territory._state s on c.state_id = s.state_id
 	inner join territory.region r on r.region_id = s.region_id
@@ -12,12 +17,12 @@ from territory.city c
 	inner join sales.saleCountry sc on co.country_id = sc.country_id
 	inner join sales.sale sl on sl.sales_id = sc.sales_id 
 		and sl.sales_lineNumber = sc.sales_lineNumber
-group by c.city_name, s.state_code, sl.sales_id
+group by c.city_name, s.state_code
 order by 'city'
 ;
-*/
 
 -- Pesquisa de produtos associadosa vendas com valor total superior a 1000
+
 
 -- numero de produtos vendidos por categoria
 select 
